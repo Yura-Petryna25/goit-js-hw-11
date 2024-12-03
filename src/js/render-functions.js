e328141031d2a02989e2552447a774d2165e0545
Yura Petryna <yur4ik911@gmail.com>
2024-12-03 13:38:01 +0200

    hw-11

---
+++ src/js/render-functions.js
@@ -0,0 +1,26 @@
+export function renderGallery(images) {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+    <div class="gallery-item">
+      <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p>Likes: ${likes}</p>
+        <p>Views: ${views}</p>
+        <p>Comments: ${comments}</p>
+        <p>Downloads: ${downloads}</p>
+      </div>
+    </div>`
+    )
+    .join('');
+}
---
+++ src/main.js
@@ -0,0 +1,48 @@
+import { fetchImages } from './js/pixabay-api';
+import { renderGallery } from './js/render-functions';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+let lightbox;
+
+async function handleSearch(event) {
+  event.preventDefault();
+  const query = form.elements.searchQuery.value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
+
+  gallery.innerHTML = '';
+  loader.classList.add('visible');
+
+  try {
+    const data = await fetchImages(query);
+    loader.classList.remove('visible');
+
+    if (data.hits.length === 0) {
+      iziToast.info({ title: 'Info', message: 'No results found.' });
+      return;
+    }
+
+    gallery.innerHTML = renderGallery(data.hits);
+
+    if (!lightbox) {
+      lightbox = new SimpleLightbox('.gallery a');
+    } else {
+      lightbox.refresh();
+    }
+  } catch (error) {
+    iziToast.error({ title: 'Error', message: 'Something went wrong!' });
+  }
+}
+form.addEventListener('submit', handleSearch);
